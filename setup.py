6e203d144c74d3e421dc93e9a16cbf9ea4955bf2
E Chow <chilledgeek@gmail.com>
2020-09-03 22:14:37 +0100

    update version and add pypi readme description

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
+from os import path
 from setuptools import setup, find_packages
 
+from requests_wrapper import __version__
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name='requests_wrapper',
-      version="0.0.1",
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      version=__version__,
       author="E CHOW",
       author_email="chilledgeek@gmail.com",
       description='Wrapper around requests package, with extra API key management',
